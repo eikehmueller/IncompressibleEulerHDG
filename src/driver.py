Fix timestep calculation

src/driver.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
 
     # number of timesteps
     h_min, _ = gridspacing(mesh)
-    nt = int(np.ceil(1 / h_min))
+    nt = int(np.ceil(args.tfinal / h_min))
     # resulting timestep size
     dt = 1 / nt
     # decay constant, kappa=0 corresponds to stationary vortex